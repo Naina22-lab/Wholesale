07ea340971f318e3c39ed976aee5b08831216ee0
naina <115720490+Naina22-lab@users.noreply.github.com>
2025-01-18 16:52:11 +0530

    app fix

--- wholesale-storage/src/App.jsx
+++ wholesale-storage/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
-import SecureWarehousing from "./components/SecureWareHousing";
+import SecureWareHousing from "./components/SecureWareHousing";
 import CompetitiveProgramming from "./components/CompetitiveProgramming";
 
 function App() {
